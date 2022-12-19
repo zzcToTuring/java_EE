d5e69301ec1beefe15bf4475c9cf58534489a856
zzc <1393407976@qq.com>
2022-12-19 18:38:43 +0800

    22-12-19 idea的使用（100%）

--- java题/demo01_hello_world.java
+++ java题/demo01_hello_world.java
@@ -1,6 +1,18 @@
+
+
 public class demo01_hello_world {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int a = 0;
+        while (a < 6) {
+            System.out.println("hello world");
+            show();
+            a++;
+        }
+    }
 
+    private static void show() {
+        System.out.println("show 断点");
+        System.out.println("输出1");
+        System.out.println("输出2");
     }
 }
